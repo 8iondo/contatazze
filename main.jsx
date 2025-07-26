6a65ce3b26829f039465979df532e38aaf7840a3
8iondo <44554381+8iondo@users.noreply.github.com>
2025-07-26 14:24:26 +0200

    fix

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BeerTrackerApp from "./BeerTrackerApp";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BeerTrackerApp />
+  </React.StrictMode>
+);
