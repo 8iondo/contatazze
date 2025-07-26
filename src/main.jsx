958505b9848b0d27f780d4e6eb9d99c30cbafe22
8iondo <44554381+8iondo@users.noreply.github.com>
2025-07-26 14:01:51 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BeerTrackerApp from "./BeerTrackerApp";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import BeerTrackerApp from './BeerTrackerApp';
+import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BeerTrackerApp />
   </React.StrictMode>
